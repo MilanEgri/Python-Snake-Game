09a8dc1c95a71ffecc7277581c4759c98b7ce8e3
Milan Egri <117757061+MilanEgri@users.noreply.github.com>
2023-12-27 17:27:47 +0100

    make a basic body for the snake

--- main.py
+++ main.py
@@ -6,7 +6,18 @@ screen.setup(600,600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
-
+turtles =[]
+positions = []
+def init_game():
+    for i in range(3):
+        turtle = Turtle()
+        turtle.shape("square")
+        turtle.color("white")
+        turtle.penup()
+        turtle.goto(20-(i*20),0)
+        positions.append(turtle.position())
+        turtles.append(turtle)
+init_game()
 
 
 screen.exitonclick()
