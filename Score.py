73fab1a6c76a7f11d67064636e94f9bd5602b1f3
Milan Egri <117757061+MilanEgri@users.noreply.github.com>
2023-12-30 13:46:22 +0100

    add record function and press escape to exit text

--- Score.py
+++ Score.py
@@ -5,17 +5,31 @@ class Score(turtle.Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.record =0
         self.hideturtle()
         self.color("white")
         self.penup()
-        self.goto(0, 265)
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 25, 'bold'))
+        ##self.goto(0, 265)
+        ##self.write(f"Score: {self.score}", align="left", font=("Arial", 25, 'bold'))
+        ##self.write(f"Record: {self.record}", align="right", font=("Arial", 25, 'bold'))
+        self.displayScore()
 
     def getScore(self):
         self.score += 1
-        self.clear()
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 25, 'bold'))
+        if self.score > self.record:
+            self.record =  self.score
+        self.displayScore()
     def restart(self):
         self.score =0
+        self.displayScore()
+    def displayScore(self):
         self.clear()
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 25, 'bold'))
+        self.goto(-280, 265)
+        self.write(f"Score: {self.score}", align="left", font=("Arial", 25, 'bold'))
+
+        self.goto(280, 265)
+        self.write(f"Record: {self.record}", align="right", font=("Arial", 25, 'bold'))
+
+        self.goto(0, 275)
+        self.write("Press Escape to Exit Game", align="center", font=("Arial", 10, 'normal'))
+
